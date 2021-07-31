e81e44d7a330878fe187a79f26b0ebdb73b14da0
Stephen Leigh <s-leigh@users.noreply.github.com>
2021-07-31 10:47:31 +0100

    Use pointers for dealing hand

--- src/hand/hand.go
+++ src/hand/hand.go
@@ -1,11 +1,16 @@
 package hand
 
-import "../deck"
+import (
+	"../deck"
+)
+
+const handSize = 2
 
 type Hand []deck.Card
 
-func DealHand(shuffledDeck deck.Deck) Hand {
-	cards := shuffledDeck[:3]
+func DealHand(shuffledDeck *deck.Deck) Hand {
+	cards := (*shuffledDeck)[:handSize-1]
+	*shuffledDeck = (*shuffledDeck)[handSize:]
 	var hand Hand
 	for _, card := range cards {
 		hand = append(hand, card)
--- src/hand/hand_test.go
+++ src/hand/hand_test.go
@@ -12,12 +12,21 @@ func TestDeck(t *testing.T) {
 	t.Run("DealHand returns hand", func(t *testing.T) {
 		shuffledDeck := deck.ShuffledDeck()
 		expectedHand := Hand{deck.Card{Value: 10, Suit: 'H'}, deck.Card{Value: 7, Suit: 'H'}, deck.Card{Value: 'J', Suit: 'S'}, deck.Card{Value: 'Q', Suit: 'S'}}
-		hand := DealHand(shuffledDeck)
+		hand := DealHand(&shuffledDeck)
 
 		for i, card := range hand {
 			if card != expectedHand[i] {
 				t.Errorf("got %v want %v", card, expectedHand[i])
 			}
 		}
+	})
+
+	t.Run("DealHand removes cards from deck", func(t *testing.T) {
+		shuffledDeck := deck.ShuffledDeck()
+		DealHand(&shuffledDeck)
+		if len(shuffledDeck) != 50 {
+			t.Errorf("Expected deck size of 50, got %v", len(shuffledDeck))
+		}
+
 	})
 }
